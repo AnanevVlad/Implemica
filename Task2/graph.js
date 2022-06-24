9b8687028df8c1f23774b7c0fe3b4496cf7bdad6
VladAnanev <ananevvlad12@gmail.com>
2022-06-24 12:45:07 +0300

    all tasks

---
+++ Task2/graph.js
@@ -0,0 +1,8 @@
+let graph = {
+  gdansk: { bydgoszcz: 1, torun: 3 },
+  bydgoszcz: { gdansk: 1,torun: 1,warszawa: 4 },
+  torun: { gdansk: 1, bydgoszcz: 2,warszawa:1 },
+  warszawa: { bydgoszcz: 4 ,torun: 1},
+    
+  }
+  
